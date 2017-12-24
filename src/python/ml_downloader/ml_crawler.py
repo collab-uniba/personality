4c616ffcfee66d4141ae7058dabd8470d1e8b8b5
Fabio Calefato <fcalefato@gmail.com>
2017-12-24 10:06:20 +0100

    code for crawling mailing lists via the mlstats miner

--- src/python/ml_downloader/ml_crawler.py
+++ src/python/ml_downloader/ml_crawler.py
@@ -3,7 +3,7 @@ import sys
 
 from orm import SessionWrapper
 from orm import ApacheProject
-from pymlstats import Application
+from mlminer.pymlstats import Application
 
 
 def get_mailing_lists():
@@ -15,33 +15,69 @@ def get_mailing_lists():
     return mls
 
 
-def start(projects_mailing_lists):
-    SessionWrapper.load_config('cfg/setup.yml')
-    SessionWrapper.new(init=True)
+def store_mailing_lists(ml_lists, dest):
+    with(open(file=dest, mode='r')) as f:
+        f.writelines(ml_lists)
 
+
+def start(mailing_lists_f):
+    mls_all = list()
+    with(open(file=mailing_lists_f, mode='r')) as f:
+        for l in f:
+            mls_all.append(l)
+
+    mls_done = list()
+    with open(file='temp.mls', mode='a') as temp:
+        for l in temp:
+            mls_done.append(l)
+
+    projects_mailing_lists = [m for m in mls_all if m not in mls_done]
+    if not projects_mailing_lists:
+        print('All mailing lists seem to have been already parsed.'
+              'Please, manually delete file ''temp.mls'' if you want to start'
+              'downloading them again.')
+        return
+
+    temp = open(file='temp.mls', mode='a')
+    error = open(file='error.mls', mode='a')
     for user_dev_mls in projects_mailing_lists:
         log.info('Starting mining mailing lists %s' % str(user_dev_mls))
-        Application(driver='mysql',
-                    user=SessionWrapper.u,
-                    password=SessionWrapper.p[1:],
-                    dbname=SessionWrapper.db_name,
-                    host=SessionWrapper.server,
-                    url_list=user_dev_mls,
-                    report_filename='mlstats-report.log',
-                    make_report=True,
-                    be_quiet=False,
-                    force=False,
-                    web_user=None,
-                    web_password=None,
-                    compressed_dir=None,
-                    backend=None,
-                    offset=0)
+        try:
+            Application(driver='mysql',
+                        user=SessionWrapper.u,
+                        password=SessionWrapper.p[1:],
+                        dbname=SessionWrapper.db_name,
+                        host=SessionWrapper.server,
+                        url_list=user_dev_mls,
+                        report_filename='mlstats-report.log',
+                        make_report=True,
+                        be_quiet=False,
+                        force=False,
+                        web_user=None,
+                        web_password=None,
+                        compressed_dir=None,
+                        backend=None,
+                        offset=0)
+            temp.write("%s\n" % user_dev_mls)
+        except Exception as e:
+            log.error('Error parsing mailing list %s' % user_dev_mls)
+            error.write(user_dev_mls)
+
+        error.close()
+        temp.close()
 
 
 if __name__ == '__main__':
     log = logging.getLogger('ml_crawler')
     try:
-        start(get_mailing_lists())
+        mls = get_mailing_lists()
+        filename = 'git-mls.txt'
+        store_mailing_lists = (mls, filename)
+
+        SessionWrapper.load_config('cfg/setup.yml')
+        SessionWrapper.new(init=True)
+
+        start(filename)
     except KeyboardInterrupt:
-        print >>sys.stderr, "\nReceived Ctrl-C or other break signal. Exiting."
+        print ("\nReceived Ctrl-C or other break signal. Exiting.", file=sys.stderr)
 
