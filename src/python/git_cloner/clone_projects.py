6a5024d23747b1f3edb983ac0a39146273364d09
Fabio Calefato <fcalefato@gmail.com>
2017-12-24 10:03:06 +0100

    git cloning scripts

--- src/python/git_cloner/clone_projects.py
+++ src/python/git_cloner/clone_projects.py
@@ -1,76 +1,48 @@
-import getopt
 import logging
 import os
-import sys
 
-from git import Repo
-
-from gitutils.repo import RepoCloner
-from loggingcfg import initialize_logger
-from szzUtils import slugToFolderName
+from git_cloner.cloner import RepoCloner
+from git_cloner.cloner import slug_to_folder_name
+from orm.apache_tables import ApacheProject
+from orm.setup import SessionWrapper
 
 
-def start(argv):
-    project_file = 'project-list.txt'
-    destination_dir = './git_repos'
-    symlink_dir = None
+def start(projects, destination_dir='./apache_repos'):
+    log.info("Starting project cloning.")
 
-    try:
-        if not argv:
-            raise getopt.GetoptError('No arguments passed from the command line. See help instructions.')
-        opts, args = getopt.getopt(argv, "hf:t:s:", ["from=", "to=", "symlink=", "help"])
-        for opt, arg in opts:
-            if opt in ("-h", "--help"):
-                print('Usage:\n clone_projects.py -f|--from=<file> -t|--to=<dir> [-s|--symlink=<dir>]')
-                sys.exit(0)
-            elif opt in ("-f", "--from"):
-                project_file = arg
-                if not os.path.exists(project_file):
-                    logger.error('%s does not exist.' % project_file)
-            elif opt in ("-t", "--to"):
-                destination_dir = os.path.abspath(arg)
-                os.makedirs(destination_dir, exist_ok=True)
-            elif opt in ("-s", "--symlink"):
-                symlink_dir = os.path.abspath(arg)
-                os.makedirs(symlink_dir, exist_ok=True)
-            else:
-                assert False, "unhandled option"
-    except getopt.GetoptError as err:
-        # print help information and exit:
-        logger.error(err)  # will print something like "option -a not recognized"
-        print('Usage:\n clone_projects.py -f|--from=<file> -t|--to=<dir> [-s|--symlink=<dir>]')
-        sys.exit(1)
+    for slug in projects:
+        slug = slug.strip()
+        s2f = slug_to_folder_name(slug)
+        dest = os.path.join(destination_dir, s2f)
 
-    logging.info("Starting project cloning.")
-    with open(project_file, 'r') as projects:
-        for slug in projects:
-            slug = slug.strip()
-            s2f = slugToFolderName(slug)
-            dest = os.path.join(destination_dir, s2f)
-
-            if os.path.exists(dest):
-                logger.info(
-                    'Project {0} already available locally in {1}, performing an update.'.format(slug, dest))
-                try:
-                    RepoCloner.pull(dest)
-                    RepoCloner.update_submodules(dest)
-                except:
-                    logger.error("Unknown error updating git repo in folder %s" % dest)
-            else:
-                RepoCloner.clone(slug, destination_dir)
-                logger.info('Project repository {0} cloned into {1}'.format(slug, s2f))
+        if os.path.exists(dest):
+            log.info(
+                'Project {0} already available locally in {1}, performing an update.'.format(slug, dest))
+            try:
+                RepoCloner.pull(dest)
                 RepoCloner.update_submodules(dest)
-            if symlink_dir:
-                try:
-                    sym = os.path.join(symlink_dir, s2f)
-                    os.symlink(dest, sym)
-                except FileExistsError:
-                    logger.debug('Symlink already existing.')
-                    pass
+            except:
+                log.error("Error updating git repo in folder %s" % dest)
+        else:
+            try:
+                RepoCloner.clone(slug, destination_dir)
+                log.info('Project repository {0} cloned into {1}'.format(slug, s2f))
+                RepoCloner.update_submodules(dest)
+            except:
+                log.error('Error cloning repo {0} into {1}'.format(slug, s2f))
 
-    logging.info('Done.')
+    log.info('Done.')
+
+
+def get_projects():
+    SessionWrapper.load_config('../apache_crawler/orm/cfg/setup.yml')
+    session = SessionWrapper.new(init=False)
+
+    projects = session.query(ApacheProject.name).filter_by(repository_type='git').all()
+    return projects
 
 
 if __name__ == '__main__':
-    logger = initialize_logger('git_cloner')
-    start()
+    log = logging.getLogger('git_cloner')
+    git_projects = get_projects()
+    start(git_projects)
---
+++ src/python/git_cloner/cloner.py
@@ -0,0 +1,48 @@
+import logging
+import os
+
+import pygit2
+from git import Repo
+
+log = logging.getLogger('RepoCloner')
+
+
+def slug_to_folder_name(slug):
+    if slug is None:
+        return None
+    return '_____'.join(slug.split('/'))
+
+
+class RepoCloner:
+
+    @staticmethod
+    def clone(slug, repos_folder):
+        try:
+            path = os.path.join(repos_folder, slug_to_folder_name(slug))
+            owner, repo = slug.split('/')
+            url = 'https://github.com/{0}/{1}.git'.format(owner, repo)
+
+            log.info(msg='Cloning repo {0} into {1}.'.format(slug, path))
+            pygit2.clone_repository(url=url, path=path)
+        except Exception as e:
+            log.error('Error cloning repo {0}: {1}'.format(slug, e))
+
+    @staticmethod
+    def update_submodules(repos_folder):
+        try:
+            path = os.path.join(repos_folder, '.git')
+
+            log.info(msg='Updating submodules of repo at {0}.'.format(path))
+            repo = pygit2.Repository(path=path)
+            repo.update_submodules()
+        except Exception as e:
+            log.error('Error updating submodules at {0}'.format(e))
+
+    @staticmethod
+    def pull(dest):
+        try:
+            repo = Repo(path=dest)
+            o = repo.remotes.origin
+            o.pull()
+        except Exception as e:
+            log.error('Error pulling git repo at {0}'.format(e))
