cf716919d07e4e7f25aab3d0e631e13b7bed8fce
Fabio Calefato <fcalefato@gmail.com>
2017-12-28 14:57:20 +0100

    added external logger with error and debug files

---
+++ src/python/logger/logging_config.py
@@ -0,0 +1,29 @@
+import logging
+
+
+def get_logger(name=__file__):
+    logger = logging.getLogger(name)
+    #_format = '%(asctime)s - %(name)s - %(levelname)-8s %(message)s'
+
+    # create console handler and set level to INFO
+    handler = logging.StreamHandler()
+    handler.setLevel(logging.INFO)
+    #formatter = logging.Formatter(_format)
+    #handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    # create error file handler and set level to WARNING
+    handler = logging.FileHandler('error.log')
+    handler.setLevel(logging.WARNING)
+    #formatter = logging.Formatter(_format)
+    #handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    # create debug file handler and set level to DEBUG
+    handler = logging.FileHandler('debug.log')
+    handler.setLevel(logging.DEBUG)
+    #formatter = logging.Formatter(_format)
+    #handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    return logger
