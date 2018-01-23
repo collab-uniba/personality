7af700502343e3154969c099e0151b4cc3941435
Fabio Calefato <fcalefato@gmail.com>
2018-01-22 15:41:58 -1000

    fixed issue that exported always the overall number of email, instead of that of the month at hand

--- src/python/export_results/export.py
+++ src/python/export_results/export.py
@@ -2,7 +2,7 @@ import json
 import logging
 
 from apache_projects.orm import ApacheProject
-from commons.aliasing import load_alias_map, get_alias_ids, find_uid
+from commons.aliasing import load_alias_map
 from commons.csv_utils import CsvWriter
 from db import SessionWrapper
 from history_analyzer.orm import CommitHistoryDevProject
@@ -11,79 +11,105 @@ from personality_insights.orm import PersonalityProjectMonth
 
 
 def merge_result_by_alias(res):
-    alias_map = load_alias_map('../unmasking/idm/dict/alias_map.dict')
-    seen_ids = set()
-    new_dicts_dict = dict()
-    for _r in res:
-        if _r['uid'] in seen_ids:
-            continue
-        aliases = get_alias_ids(alias_map, _r['uid']) + [_r['uid'], ]
-        seen_ids = set(list(seen_ids) + aliases)
+    # uids are already unaliased
+    uids_prjs = set(sorted((_r['uid'], _r['project']) for _r in res))
+    new_dicts = list()
+    for uid, prj in uids_prjs:
+        sel = [_r for _r in res if _r['uid'] == uid and _r['project'] == prj]
 
-        #uid = find_uid(alias_map, aliases)
-        uid = alias_map[_r['uid']]
-        if uid in new_dicts_dict.keys():
-            new_dict = new_dicts_dict[uid]
-        else:
+        if sel:
             new_dict = dict()
-            new_dicts_dict[uid] = new_dict
+            new_dicts.append(new_dict)
             new_dict['uid'] = uid
-            new_dict['project'] = _r['project']
-            new_dict['project_status'] = _r['project_status']
-            new_dict['project_language'] = _r['project_language']
-            new_dict['project_category'] = _r['project_category']
+            new_dict['project'] = prj
+            new_dict['project_status'] = sel[0]['project_status']
+            new_dict['project_language'] = sel[0]['project_language']
+            new_dict['project_category'] = sel[0]['project_category']
 
-        for a in aliases:
-            try:
-                #old_dict = [x for x in res if x['uid'] == alias_map[a]][0]
+            new_dict['author_track_record_days'] = 0
+            new_dict['num_authored_commits'] = 0
+            new_dict['num_integrated_commits'] = 0
+            new_dict['integrator_track_record_days'] = 0
+            new_dict['first_authored_datetime'] = None
+            new_dict['last_authored_datetime'] = None
+            new_dict['first_integrated_datetime'] = None
+            new_dict['last_integrated_datetime'] = None
+            new_dict['tot_num_additions_authored'] = 0
+            new_dict['tot_num_deletions_authored'] = 0
+            new_dict['tot_num_files_changed_authored'] = 0
+            new_dict['tot_src_loc_added_authored'] = 0
+            new_dict['tot_src_loc_deleted_authored'] = 0
+            new_dict['tot_src_files_touched_authored'] = 0
 
-                new_dict['author_track_record_days'] += _r['author_track_record_days']
-                new_dict['num_authored_commits'] += _r['num_authored_commits']
-                new_dict['num_integrated_commits'] += _r['num_integrated_commits']
-                new_dict['integrator_track_record_days'] += _r['integrator_track_record_days']
-                new_dict['first_authored_datetime'] = min(new_dict['first_authored_datetime'],
-                                                          _r['first_authored_datetime'])
-                new_dict['last_authored_datetime'] = max(new_dict['last_authored_datetime'],
-                                                         _r['last_authored_datetime'])
-                new_dict['first_integrated_datetime'] = min(new_dict['first_integrated_datetime'],
-                                                            _r['first_integrated_datetime'])
-                new_dict['last_integrated_datetime'] = max(new_dict['last_integrated_datetime'],
-                                                           _r['last_integrated_datetime'])
+            new_dict['tot_num_additions_integrated'] = 0
+            new_dict['tot_num_deletions_integrated'] = 0
+            new_dict['tot_num_files_changed_integrated'] = 0
+            new_dict['tot_src_loc_added_integrated'] = 0
+            new_dict['tot_src_loc_deleted_integrated'] = 0
+            new_dict['tot_src_files_touched_integrated'] = 0
 
-                new_dict['tot_num_additions_authored'] += _r['tot_num_additions_authored']
-                new_dict['tot_num_deletions_authored'] += _r['tot_num_deletions_authored']
-                new_dict['tot_num_files_changed_authored'] += _r['tot_num_files_changed_authored']
-                new_dict['tot_src_loc_added_authored'] += _r['tot_src_loc_added_authored']
-                new_dict['tot_src_loc_deleted_authored'] += _r['tot_src_loc_deleted_authored']
-                new_dict['tot_src_files_touched_authored'] += _r['tot_src_files_touched_authored']
+            new_dict['is_author'] = False
+            new_dict['is_integrator'] = False
 
-                new_dict['tot_num_additions_integrated'] += _r['tot_num_additions_integrated']
-                new_dict['tot_num_deletions_integrated'] += _r['tot_num_deletions_integrated']
-                new_dict['tot_num_files_changed_integrated'] += _r['tot_num_files_changed_integrated']
-                new_dict['tot_src_loc_added_integrated'] += _r['tot_src_loc_added_integrated']
-                new_dict['tot_src_loc_deleted_integrated'] += _r['tot_src_loc_deleted_integrated']
-                new_dict['tot_src_files_touched_integrated'] += _r['tot_src_files_touched_integrated']
+            for s in sel:
+                new_dict['author_track_record_days'] += s['author_track_record_days']
+                new_dict['num_authored_commits'] += s['num_authored_commits']
+                new_dict['num_integrated_commits'] += s['num_integrated_commits']
+                new_dict['integrator_track_record_days'] += s['integrator_track_record_days']
+                if s['first_authored_datetime'] and new_dict['first_authored_datetime']:
+                    new_dict['first_authored_datetime'] = min(new_dict['first_authored_datetime'],
+                                                              s['first_authored_datetime'])
+                else:
+                    new_dict['first_authored_datetime'] = s['first_authored_datetime']
+                if new_dict['last_authored_datetime'] and s['last_authored_datetime']:
+                    new_dict['last_authored_datetime'] = max(new_dict['last_authored_datetime'],
+                                                             s['last_authored_datetime'])
+                else:
+                    new_dict['last_authored_datetime'] = s['last_authored_datetime']
+                if new_dict['first_integrated_datetime'] and s['first_integrated_datetime']:
+                    new_dict['first_integrated_datetime'] = min(new_dict['first_integrated_datetime'],
+                                                                s['first_integrated_datetime'])
+                else:
+                    new_dict['first_integrated_datetime'] = s['first_integrated_datetime']
+                if new_dict['last_integrated_datetime'] and s['last_integrated_datetime']:
+                    new_dict['last_integrated_datetime'] = max(new_dict['last_integrated_datetime'],
+                                                               s['last_integrated_datetime'])
+                else:
+                    new_dict['last_integrated_datetime'] = s['last_integrated_datetime']
 
-                new_dict['is_author'] = new_dict['is_author'] or _r['is_author']
-                new_dict['is_integrator'] = new_dict['is_integrator'] or _r['is_integrator']
-            except:
-                continue
+                new_dict['tot_num_additions_authored'] += s['tot_num_additions_authored']
+                new_dict['tot_num_deletions_authored'] += s['tot_num_deletions_authored']
+                new_dict['tot_num_files_changed_authored'] += s['tot_num_files_changed_authored']
+                new_dict['tot_src_loc_added_authored'] += s['tot_src_loc_added_authored']
+                new_dict['tot_src_loc_deleted_authored'] += s['tot_src_loc_deleted_authored']
+                new_dict['tot_src_files_touched_authored'] += s['tot_src_files_touched_authored']
 
-    return new_dicts_dict.values()
+                new_dict['tot_num_additions_integrated'] += s['tot_num_additions_integrated']
+                new_dict['tot_num_deletions_integrated'] += s['tot_num_deletions_integrated']
+                new_dict['tot_num_files_changed_integrated'] += s['tot_num_files_changed_integrated']
+                new_dict['tot_src_loc_added_integrated'] += s['tot_src_loc_added_integrated']
+                new_dict['tot_src_loc_deleted_integrated'] += s['tot_src_loc_deleted_integrated']
+                new_dict['tot_src_files_touched_integrated'] += s['tot_src_files_touched_integrated']
+
+                new_dict['is_author'] = new_dict['is_author'] or s['is_author']
+                new_dict['is_integrator'] = new_dict['is_integrator'] or s['is_integrator']
+
+    return new_dicts
 
 
 def save_personality_results():
     global rows, r, r_dict
     personality_filename = 'personality.csv'
     logger.info('Exporting personality data to file %s' % personality_filename)
-    personality_header = ['uid', 'project', 'month', 'email_count',
+    personality_header = ['uid', 'project', 'month', 'email_count', 'word_count',
                           'opennes', 'agreeableness', 'neuroticism', 'extraversion', 'conscientiousness',
                           'opennes_percentile', 'agreeableness_percentile', 'neuroticism_percentile',
                           'extraversion_percentile', 'conscientiousness_percentile']
     personality_writer = CsvWriter(personality_filename, personality_header, 'w')
     rows = session.query(PersonalityProjectMonth).order_by(PersonalityProjectMonth.dev_uid,
                                                            PersonalityProjectMonth.project_name,
-                                                           PersonalityProjectMonth.month).all()
+                                                           PersonalityProjectMonth.month,
+                                                           PersonalityProjectMonth.word_count).all()
     if rows:
         for r in rows:
             r_dict = dict()
@@ -91,6 +117,7 @@ def save_personality_results():
             r_dict['project'] = r.project_name
             r_dict['month'] = r.month
             r_dict['email_count'] = r.email_count
+            r_dict['word_count'] = r.word_count
             try:
                 scores = json.loads(r.scores.replace('\'', '"').replace('True', '"True"'))
             except json.decoder.JSONDecodeError:
@@ -139,7 +166,7 @@ def save_commit_results():
                 logger.debug('Skipped %s for project %s, nothing authored or integrated' % (r.dev_uid, r.project_name))
                 continue
             r_dict = dict()
-            r_dict['uid'] = r.dev_uid
+            r_dict['uid'] = alias_map[r.dev_uid]
             r_dict['project'] = r.project_name
             res = session.query(ApacheProject.language, ApacheProject.status, ApacheProject.category).filter_by(
                 name=r.project_name).one()
@@ -173,7 +200,8 @@ def save_commit_results():
 
             entry_list.append(r_dict)
 
-    #entry_list = merge_result_by_alias(entry_list)
+    # TODO
+    entry_list = merge_result_by_alias(entry_list)
     commit_writer.writerows(entry_list)
     commit_writer.close()
     logger.info('Done')
@@ -183,6 +211,7 @@ if __name__ == '__main__':
     logger = logging_config.get_logger('export', console_level=logging.DEBUG)
     SessionWrapper.load_config('../db/cfg/setup.yml')
     session = SessionWrapper.new(init=True)
+    alias_map = load_alias_map('../unmasking/idm/dict/alias_map.dict')
 
     save_personality_results()
     save_commit_results()
