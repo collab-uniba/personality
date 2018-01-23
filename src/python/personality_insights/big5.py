6408321cb3c83ef0d5f824cbb189cc3f93599a94
Fabio Calefato <fcalefato@gmail.com>
2018-01-22 15:41:00 -1000

    fixed issue with email query

--- src/python/personality_insights/big5.py
+++ src/python/personality_insights/big5.py
@@ -1,20 +1,22 @@
 import logging
 import re
 import sys
-from _datetime import datetime
+from datetime import datetime
 from itertools import groupby
 
 from bs4 import BeautifulSoup as BS4
 from requests.exceptions import *
+from sqlalchemy import exc
 from sqlalchemy import func
 from sqlalchemy import or_, and_
-from sqlalchemy.orm import exc
+from sqlalchemy import orm
 from watson_developer_cloud import WatsonException, WatsonInvalidArgument
 
 from apache_projects.orm.apache_tables import *
 from commit_analyzer.orm.commit_tables import *
 from commons.aliasing import load_alias_map, get_alias_ids
 from db.setup import SessionWrapper
+from history_analyzer.orm import CommitHistoryDevProject
 from logger import logging_config
 from ml_downloader.orm.mlstats_tables import *
 from personality_insights import big5_personality
@@ -51,23 +53,21 @@ def clean_up(message_bodies):
             soup = BS4(message_body, 'html.parser')
             clean_message_body = soup.text
         except Exception as e:
-            logger.error('Error with BS4 on text:\n\n%s\n\n' % message_body, e)
-            clean_message_body = message_body
+            logger.error('Error with BS4 on text:\n\n%s\n\n' % message_body, str(e))
+            clean_message_body = message_body.strip()
 
         clean_message_body = re.sub(r'^\s*>+( .*)?', '', clean_message_body, flags=re.MULTILINE)
-        # clean_message_body = re.sub(r'^\s*\$+( .*)?', '', clean_message_body, flags=re.MULTILINE)
         clean_message_body = re.sub(r'^\s*\+', '', clean_message_body, flags=re.MULTILINE)
         clean_message_body = re.sub(r'^\s*---\+', '', clean_message_body, flags=re.MULTILINE)
-        # dates
-        clean_message_body = re.sub(r'https?:\/\/\S*', '', clean_message_body, flags=re.MULTILINE)
-        clean_message_body = re.sub(r'[\w\.-]+ @ [\w\.-]+', '', clean_message_body, flags=re.MULTILINE)
-        clean_message_body = re.sub(r'On .* wrote:.*', '', clean_message_body, flags=re.MULTILINE)
         clean_message_body = re.sub(r'\n[\t\s]*\n+', '', clean_message_body, flags=re.MULTILINE)
-        clean_message_body = re.sub(r'[\n+]Sent from', '', clean_message_body, flags=re.MULTILINE)
         clean_message_body = re.sub(r'({+|}+|\++|_+|=+|-+|\*|\\+|/+|@+|\[+|\]+|:+|<+|>+|\(+|\)+)', '',
                                     clean_message_body, flags=re.MULTILINE)
-
+        clean_message_body = re.sub(r'On .* wrote:.*', '', clean_message_body, flags=re.MULTILINE)
+        clean_message_body = re.sub(r'[\n+]Sent from', '', clean_message_body, flags=re.MULTILINE)
+        clean_message_body = re.sub(r'https?:\/\/\S*', '', clean_message_body, flags=re.MULTILINE)
+        clean_message_body = re.sub(r'[\w\.-]+ @ [\w\.-]+', '', clean_message_body, flags=re.MULTILINE)
         # clean_message_body = clean_message_body.encode('utf-8').strip()
+
         cleansed.append(clean_message_body.strip())
     return cleansed
 
@@ -82,13 +82,13 @@ def get_alias_email_addresses(alias_ids):
                 try:
                     res = session.query(GithubDeveloper.email).filter_by(id=alias_id).one()
                     alias_email_addresses.add(res.email)
-                except (exc.NoResultFound, exc.MultipleResultsFound):
+                except (orm.exc.NoResultFound, orm.exc.MultipleResultsFound):
                     continue
             else:  # from MailingListSenderId - people_id
                 try:
                     res = session.query(MailingListSenderId.email_address).filter_by(id=alias_id).one()
                     alias_email_addresses.add(res.email_address)
-                except (exc.NoResultFound, exc.MultipleResultsFound):
+                except (orm.exc.NoResultFound, orm.exc.MultipleResultsFound):
                     continue
 
     return list(alias_email_addresses)
@@ -96,9 +96,11 @@ def get_alias_email_addresses(alias_ids):
 
 def get_all_emails(email_addresses, mailing_lists):
     # for address in email_addresses:
-    res = session.query(Messages.first_date, Messages.message_body).filter(
-        and_(or_(Messages.mailing_list_url == ml for ml in mailing_lists),
-             or_(MessagesPeople.email_address == e for e in email_addresses))).distinct().all()
+    res = session.query(MessagesPeople.message_id, Messages.subject, Messages.first_date,
+                        Messages.message_body).filter_by(type_of_recipient='From',
+                                                         message_id=Messages.message_id).filter(
+        and_(or_(MessagesPeople.email_address == e for e in email_addresses),
+             or_(Messages.mailing_list_url == ml for ml in mailing_lists))).distinct().all()
     return res
 
 
@@ -109,23 +111,30 @@ def get_personality_score_by_month(uid, p_name, usr_emails, resume_month):
     for month, eml_list in groupby(usr_emails, key=lambda e: datetime.strftime(e.first_date, "%Y-%m")):
         if resume_month is not None and month <= resume_month:
             continue
-        logger.debug('Processing month %s' % month)
+        logger.debug('Cleaning up email bodies')
         clean_emails = clean_up([x.message_body for x in eml_list])
+        logger.info('Getting personality scores for month %s' % month)
         json_score = get_profile('\n\n'.join(clean_emails))
+        del clean_emails
         # store in table; keys: dev_uid, project_name, month
         if json_score is not None and json_score != '':
             word_count = json_score['word_count']
         else:
             word_count = 0
         v = PersonalityProjectMonth(dev_uid=uid, project_name=p_name, month=month,
-                                    email_count=len(usr_emails), word_count=word_count,
+                                    email_count=len(clean_emails), word_count=word_count,
                                     scores=json_score)
         logger.debug('Adding %s' % v)
         session.add(v)
         try:
             session.commit()
+            del v
+            del json_score
+        except exc.IntegrityError:
+            logger.warning('Duplicate entry for %s, rolling back session and keep going', v)
+            session.rollback()
         except Exception as e:
-            logger.error('Error storing personality scores for %s' % v, e)
+            logger.error('Unknown error storing personality scores for %s\n\n' % v, e)
             continue
 
 
@@ -145,7 +154,7 @@ def already_parsed_uid_project_month(ids, p_name):
 def already_parsed_uid_project(ids):
     res = session.query(PersonalityProjectMonth.project_name).filter(
         or_(PersonalityProjectMonth.dev_uid == _id for _id in ids)).order_by(
-        PersonalityProjectMonth.project_name.desc()).all()
+        PersonalityProjectMonth.project_name.desc()).distinct().all()
     if res:
         p_name = res[0][0]
     else:
@@ -153,9 +162,8 @@ def already_parsed_uid_project(ids):
     return p_name
 
 
-def already_parsed_uid(ids):
-    res = session.query(func.max(PersonalityProjectMonth.dev_uid)).filter(
-        or_(PersonalityProjectMonth.dev_uid == _id for _id in ids)).all()
+def already_parsed_uid():
+    res = session.query(func.max(PersonalityProjectMonth.dev_uid)).all()
     uid = res[0][0]
     return uid
 
@@ -167,24 +175,31 @@ def main():
                       filter(
         and_(ApacheProject.name == GithubRepository.slug, GithubRepository.id == Commit.repo_id)).distinct().all())
 
+    contributors = session.query(CommitHistoryDevProject).order_by(CommitHistoryDevProject.dev_uid).distinct().all()
+
+    contributors_set = sorted(set([alias_map[x.dev_uid] for x in contributors]))
+
+    resume_id = already_parsed_uid()
     for uid in sorted(set(alias_map.values())):
-        aliases = sorted(get_alias_ids(alias_map, uid) + [uid, ])
         # negative ids for ASFers
         # positive for git developers
         # positive, starts from OFFSET ids for emailers
-        resume_id = already_parsed_uid(aliases)
         if resume_id is not None and uid < resume_id:
             logger.debug('%s already analyzed, skipping' % uid)
             continue
+        if uid not in contributors_set:
+            logger.debug('%s did not contribute to any project\'s code base, skipping' % uid)
+            continue
 
+        aliases = sorted(get_alias_ids(alias_map, uid) + [uid, ])
         alias_email_addresses = get_alias_email_addresses(aliases)
         if not alias_email_addresses:
             logger.debug('%s has no email addresses associated, skipping' % uid)
             continue
         logger.info('Processing uid %s <%s>' % (uid, ','.join(alias_email_addresses)))
 
+        resume_project = already_parsed_uid_project(aliases)
         for p in projects:
-            resume_project = already_parsed_uid_project(aliases)
             if resume_project is not None and p.name < resume_project:
                 continue
             logger.info('Processing project %s' % p.name)
@@ -197,6 +212,7 @@ def main():
             if all_emails:
                 resume_month = already_parsed_uid_project_month(aliases, p.name)
                 get_personality_score_by_month(uid, p.name, all_emails, resume_month)
+                del all_emails
             else:
                 logger.debug(
                     'No emails from %s <%s> to project \'%s\' mailing lists' % (uid, alias_email_addresses, p.name))
