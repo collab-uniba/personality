63fa60fe6f64b4eacfc0b916a831ac27edfb3bf5
Fabio Calefato <fcalefato@gmail.com>
2018-08-07 12:52:49 +0200

    Fix mispelled param value

--- src/python/big5_personality/run.sh
+++ src/python/big5_personality/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export PYTHONPATH=$PYTHONPATH:../:../apache_projects
-# mandatory param: tool name, either liwc or pi
+# mandatory param: tool name, either liwc or p_insights
 # optional param: reset, to empty the db tables containing personality data
 python big5.py $@
